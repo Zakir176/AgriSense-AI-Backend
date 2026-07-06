c500b3456ba1c6a558489f9fc640ab716e480cd3
Zakir Motala <zakirhusseinmotala76@gmail.com>
2026-07-06 22:00:45 +0200

    feat(backend): link user_farms relation to User model

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from ..database import Base
 
 class User(Base):
@@ -8,3 +9,5 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=True)
+
+    user_farms = relationship("UserFarmAssociation", back_populates="user", cascade="all, delete-orphan")
